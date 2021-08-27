f47aa94bba2f046de50ef230e550890e778f6115
aditya-rawat773 <adityarawat.cf@gmail.com>
2021-08-28 00:28:35 +0530

    login layout

---
+++ app/src/main/java/com/example/mirrorscore/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mirrorscore.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mirrorscore.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth_acticvity)
+    }
+}
